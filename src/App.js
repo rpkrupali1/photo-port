5862879d3ffe58371dbdaa97d11c01a7592a82d5
krupali pilgulwar <rpkrupali@gmail.com>
2022-04-09 16:15:05 -0500

    handle submit data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import About from "./components/About";
 import Nav from "./components/Nav";
+import ContactForm from "./components/Contact";
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <ContactForm></ContactForm>
         <About> </About>
       </main>
     </div>
